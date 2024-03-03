7dbf53a9fea980b8b98e9f006d61440ba7d52b70
Ademola Ogunle <ogunleademola@gmail.com>
2024-03-03 18:24:29 +0100

    make some color adjustment

--- component/Discover.jsx
+++ component/Discover.jsx
@@ -1,9 +1,9 @@
-import { LuSendHorizonal } from "react-icons/lu";
+// import { LuSendHorizonal } from "react-icons/lu";
 
 export const Discover = () => {
   return (
     <section className='flex w-full flex-col justify-center text-center my-12'>
-        <h1 className='text-4xl leading-snug font-semibold text-black/80'>
+        <h1 className='text-4xl leading-snug font-semibold text-black/80 dark:text-gray-200'>
             Discover Your Signature 
             <br/>
             <span className='text-green-500/40'>Style</span> & <span className='text-pink-400/40'>Elevate</span> Your
@@ -18,7 +18,7 @@ export const Discover = () => {
         <form className='border-black mt-5 border w-6/12 self-center rounded-xl py-4 px-4 flex justify-between'>
             <input type="text" placeholder='Send us a message' required className='outline-none bg-transparent pr-10'/>
             <button type='submit'>
-              <LuSendHorizonal  />
+              {/* <LuSendHorizonal  /> */}
             </button>
         </form>
     </section>
--- component/pages/About.jsx
+++ component/pages/About.jsx
@@ -4,18 +4,18 @@ import logo from '@/public/asset/Ark.jpg'
 
 const About = () => {
   return (
-    <section className='bg-gray-200/75 min-h-max py-12 mt-12'>
+    <section className='bg-gray-200/75 dark:bg-black min-h-max py-12 mt-12'>
       <div className='px-12 flex justify-between items-center'>
-        <div className="border-[8px] border-black/60 h-auto mr-12 w-4/12">
+        <div className="border-[8px] border-gray-50/25 h-auto mr-12 w-4/12 rounded-md">
           <Image src={pic2} alt="ads" className="object-cover aspect-[1/2]"/>
         </div>
 
         <div className="w-full">
-          <div className="rounded-full h-8 w-8 overflow-hidden relative">
-           <Image src={logo} alt='logo' fill className='object-conver'/>
+          <div className="rounded-full h-12 w-12 overflow-hidden relative">
+           <Image src={logo} alt='logo' fill className='object-cover'/>
           </div>
-          <h3 className="text-xl text-black/75 font-bold my-4">About Ark Stitches</h3>
-          <p className="text-balance">
+          <h3 className="text-2xl text-black/75 font-bold my-4 dark:text-gray-200">About Ark Stitches</h3>
+          <p className="text-balance dark:text-gray-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatum in cum quia eum, tenetur corporis illum soluta. Fuga distinctio unde laborum commodi numquam deserunt expedita ut quae, natus itaque ipsa hic dicta ex impedit neque, consequuntur quia officiis nulla dolorum omnis, reprehenderit possimus! Doloremque voluptatem at architecto, ducimus ratione eius voluptates debitis rem itaque mollitia officiis placeat? Reprehenderit non exercitationem nam sed et dolorem iste quae consequuntur consectetur ducimus eligendi maxime reiciendis sint, ex quam id eveniet? Error doloremque repellat provident tempora non, asperiores atque omnis odio ullam corrupti soluta. Nulla quae est dolore ipsam nostrum libero quod natus!
           </p>
         </div>
