ac4fd8ca67c1f9775367f82292f9a59c469b962c
Ademola Ogunle <ogunleademola@gmail.com>
2024-03-03 13:06:03 +0100

    added logo to navbar

--- componenet/Navbar.jsx
+++ componenet/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Image from 'next/image'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between items-center w-full bg-white'>
+      <section className='w-4/12'>
+        <Image src='../asset/Ark.jpg' alt='logo '/>
+      </section>
+      <section className='w-4/12 bg-teal'></section>
+      <section className='w-4/12 bg-green'></section>
+    </nav>
   )
 }
