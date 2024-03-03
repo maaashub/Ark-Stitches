183cc477e8a271c7465fd890042f62b7925cb77d
Adetimmy <ogunleademola@gmail.com>
2024-03-03 15:43:15 +0100

    added the gallery component with dummies

--- component/pages/Gallery.jsx
+++ component/pages/Gallery.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Image from "next/image";
+import pic from "@/public/asset/1.jpg";
+import pic1 from "@/public/asset/2.png";
+import pic2 from "@/public/asset/3.jpg";
+import pic3 from "@/public/asset/4.png";
+import pic4 from "@/public/asset/5.jpg";
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <section className="mt-12 grid grid-cols-5 auto-rows-[300px] gap-3 px-12">
+      <div className="relative row-span-2 rounded-xl overflow-hidden">
+        <Image src={pic} alt="ads" fill className="object-cover"/>
+        <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative rounded-xl overflow-hidden">
+        <Image src={pic1} alt="ads" fill className="object-cover" />
+        <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative row-span-2 rounded-xl overflow-hidden">
+        <Image src={pic2} alt="ads" fill className="object-cover"/>
+                <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative rounded-xl overflow-hidden">
+        <Image src={pic3} alt="ads" fill className="object-cover"/>
+                <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative row-span-2 rounded-xl overflow-hidden">
+        <Image src={pic4} alt="ads" fill className="object-cover"/>
+                <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative rounded-xl overflow-hidden">
+        <Image src={pic} alt="ads" fill className="object-cover"/>
+                <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+      <div className="relative rounded-xl overflow-hidden">
+        <Image src={pic3} alt="ads" fill className="object-cover"/>
+                <div className="flex w-full h-full items-center justify-center">
+          <button className="absolute text-white">play</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Gallery
+export default Gallery;
