6f42b232bb441f015cfbcfd44702562036a535a9
Ademola Ogunle <ogunleademola@gmail.com>
2024-03-03 14:39:20 +0100

    link attached

--- component/Button.jsx
+++ component/Button.jsx
@@ -4,6 +4,7 @@ export const Button = ({children}) => {
   return (
     <button type='button' className='bg-black/80 hover:bg-transparent  dark:hover:text-[#a5a4a4]  hover:border-black/85 border border-transparent hover:text-black transition-all rounded-2xl px-4 py-2 text-white'>
         {children}
+        https://www.instagram.com/arkstitchesabuja?igsh=dDV6anJiOWs1NWVn
     </button>
   )
 }
